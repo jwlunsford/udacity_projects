bb82f68604bd56c9d951c377337db3063ef3aec6
jwlunsford <jon.lunsford@outlook.com>
2018-04-03 23:17:42 -0500

    corrected SQL queries and streamlined code

--- logs_analysis_project/README.md
+++ logs_analysis_project/README.md
@@ -6,15 +6,31 @@ The modules included in this project are:
 
 ### Description:
 This project was developed in Python 3.  It is a server log analysis tool that connects to
-a PostgreSQL database and executes three queries. The query results are written to a plain text file called 'output.txt'.  Queries can be added or modified in the project using Python code.
-The database for the project was provided by Udacity and is not included with this module.
+a PostgreSQL database and executes three queries. The query results are written to a plain text file called 'output.txt'.  Queries can be added or modified in the project using Python code.  The project
+is provided as part of the Udacity Full Stack Developer Nanodegree.
 
-### Author:
-Jon Lunsford, March 18, 2018
+* The project sets up a mock PostgreSQL database for a fictional news website.  The Python script uses
+psycopg2 to query the database and generate output that answers the following three questions.
+  1.  What is the most popular article on the site?
+  2.  Who are the most popular article authors?
+  3.  On which days did more than 1% of the requests lead to errors?
 
-### Installation
-No installation is required to run this code, but it will not work without the database.
-The database was provided by Udacity in a Linux virtual environment.
+### Requirements
+In order to use this program, users will need to install a Linux VM with Vagrant:
+* [Vagrant](https://www.vagrantup.com/downloads.html)
+* [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
 
 ### Running:
 The code cannot be run without the associated database.
+* Creating the database:
+  * if you use the Vagrantfile supplied by Udacity, this step is automated, otherwise
+  * manually create the news database using SQL.  The following link provides the SQL file needed to recreate the database.  [newsdata.sql](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip).  To setup the news database you will need to unzip this file and follow the steps below.
+  1.  from the command line, 'cd' into the vagrant directory.
+  2.  run the 'psql' command
+  *  `psql -d news -f newsdata.sql`
+  this command will connect to your database server and create the tables and data.
+  * To learn more about how to use the psql command line interface see the [Postgres Guide](http://postgresguide.com/utilities/psql.html).
+
+
+### Author:
+Jon Lunsford, March 18, 2018
--- logs_analysis_project/log_analysis.py
+++ logs_analysis_project/log_analysis.py
@@ -2,75 +2,56 @@
 
 import psycopg2
 
+qry_strings = {'most_popular_articles':
+               """SELECT articles.title as title, count(title) as num
+                  FROM log LEFT JOIN articles on log.path
+                  ILIKE '%\' || substring(replace(regexp_replace(articles.title, 
+                  '[^a-zA-Z0-9 ]', ''), ' ', '-') from '\w+-\w+-\w+')
+                  GROUP BY title
+                  ORDER BY num desc limit 3;""",
+               'most_popular_authors':
+               """SELECT authors.name, count(*) AS cnt
+                  FROM articles INNER JOIN authors ON
+                  articles.author = authors.id INNER JOIN
+                  log on log.path = concat('/article/', articles.slug)
+                  WHERE log.status = '200 OK'
+                  GROUP BY authors.name
+                  ORDER BY cnt desc;""",
+                'errors_gt_one_pct':
+                """SELECT daily404.day1,
+                   (daily404.freq/requests.totreq::float)*100 AS pct
+                   FROM ((SELECT log.time::date as day1, count(*) AS freq
+                   FROM log WHERE status LIKE '404%' GROUP BY day1
+                   ) daily404
+                   FULL JOIN (SELECT log.time::date AS day2, count(status) AS
+                   totreq FROM log GROUP BY day2
+                   ) requests
+                   ON daily404.day1 = requests.day2)
+                   WHERE daily404.freq/requests.totreq::float > 0.01;"""
+               }
 
-def most_popular_articles():
-    '''create and execute a query that finds the three most popular
-    articles'''
+
+def run_query(qry):
+    '''create and execute a query in PostgresSQL connection
+    :param qry:  a qualified query string.
+    '''
     # connect to the database
     db = psycopg2.connect("dbname=news")
     # create a cursor
     cur = db.cursor()
-    qryparts = ["SELECT path, count(path) AS cnt FROM log GROUP BY path",
-                "HAVING path LIKE '/article%' ORDER BY cnt desc limit 3;"]
-    # rejoin the qryparts list
-    this_qry = ' '.join(qryparts)
-    # execute the desired query
-    cur.execute(this_qry)
-    rst = cur.fetchall()
-    # close the connection and return the result
-    db.close()
-    return rst
-
-
-def most_popular_author():
-    '''create and execute a query that finds the most popular authors
-    by the total number of articles they've written.'''
-    # connect to the database
-    db = psycopg2.connect("dbname=news")
-    # create a cursor
-    cur = db.cursor()
-    qryparts = ["SELECT authors.name, count(*) AS cnt"
-                "FROM articles INNER JOIN authors ON",
-                "articles.author = authors.id INNER JOIN",
-                "log on log.path = concat('/article/', articles.slug)",
-                "WHERE log.status = '200 OK'",
-                "GROUP BY authors.name",
-                "ORDER BY cnt desc;"]
-    # rejoin the qryparts list
-    this_qry = ' '.join(qryparts)
-    # execute the desired query
-    cur.execute(this_qry)
-    rst = cur.fetchall()
-    # close the connection and return the result
-    db.close()
-    return rst
-
-
-def request_errors_gt_one_pct():
-    '''create and execute a query that finds the date or dates where the
-    404 errors are more than 1% of the total page views.'''
-    # connect to the database
-    db = psycopg2.connect("dbname=news")
-    # create a cursor
-    cur = db.cursor()
-    qryparts = ["SELECT daily404.day1, daily404.freq, requests.totreq,",
-                "(daily404.freq/requests.totreq::float)*100 AS pct",
-                "FROM ((SELECT log.time::date as day1, count(*) AS freq",
-                "FROM log WHERE status LIKE '404%' GROUP BY day1",
-                ") daily404",
-                "FULL JOIN (SELECT log.time::date AS day2, count(status) AS",
-                "totreq FROM log GROUP BY day2",
-                ") requests",
-                "ON daily404.day1 = requests.day2)",
-                "WHERE daily404.freq/requests.totreq::float > 0.01;"]
-    # rejoin the qryparts list
-    this_qry = ' '.join(qryparts)
-    # execute the query
-    cur.execute(this_qry)
-    rst = cur.fetchall()
-    # close the connection and return the result
-    db.close()
-    return rst
+    try:
+        if qry:
+            # execute the desired query
+            cur.execute(qry)
+            rst = cur.fetchall()
+            return rst
+        else:
+            print("The query value must be provided.")
+    except RuntimeException:
+        print("Error, could not execute the desired query.")
+    finally:
+        # close the connection and return the result
+        db.close()
 
 
 if __name__ == '__main__':
@@ -80,28 +61,28 @@ if __name__ == '__main__':
         # write the first heading
         f.write("3 most popular articles of all time ...")
         f.write('\n')
-        rst = most_popular_articles()
+        rst = run_query(qry_strings['most_popular_articles'])
         # write the first query results to file
         for result in rst:
-            f.write('Article: {}, {} views'.format(result[0], result[1]))
+            f.write('Article: {} -- {} views'.format(result[0], result[1]))
             f.write("\n")
         f.write('\n')
         # write the second heading
         f.write("Most popular article authors of all time ...")
         f.write('\n')
-        rst = most_popular_author()
+        rst = run_query(qry_strings['most_popular_authors'])
         # write the second query results to the file
         for result in rst:
-            f.write('Author: {}, {} views'.format(result[0], result[1]))
+            f.write('Author: {} -- {} views'.format(result[0], result[1]))
             f.write("\n")
         f.write('\n')
         # write the third heading
         f.write("Days where errors are greater than 1%...")
         f.write('\n')
-        rst = request_errors_gt_one_pct()
+        rst = run_query(qry_strings['errors_gt_one_pct'])
         # write the thrid query results to the file
         for result in rst:
-            f.write("Date: {}, {}% errors".format(result[0], result[3]))
+            f.write("Date: {} -- {}% errors".format(result[0], result[1]))
             f.write('\n')
     # close the file
     f.close()
