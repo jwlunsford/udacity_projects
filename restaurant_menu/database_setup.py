d6780ee81d697c67820adc836bfb748e36d57d57
Jon Lunsford <jon.lunsford@outlook.com>
2019-02-24 21:31:33 -0600

    feat: added Serialize property to Menu and Restaurant
    Serialize property will create a JSON style output for an API endpoint.

--- restaurant_menu/database_setup.py
+++ restaurant_menu/database_setup.py
@@ -15,18 +15,27 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-    
+
     __tablename__ = 'restaurant'
-    
+
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
-    
-    
-    
+
+    @property
+    def Serialize(self):
+        # create a JSON style object for an API endpoint
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
+
+
+
 class MenuItem(Base):
-    
+
     __tablename__ = 'menu_item'
-    
+
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     course = Column(String(250))
@@ -34,7 +43,18 @@ class MenuItem(Base):
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-    
+
+    @property
+    def Serialize(self):
+        # create a JSON style object for an API endpoint
+        return {
+            'name': self.name,
+            'id': self.id,
+            'course': self.course,
+            'description': self.description,
+            'price': self.price,
+        }
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 
