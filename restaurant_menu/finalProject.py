f64ca772170d0a9929f3099aaf582008ac1c8a2f
Jon Lunsford <jon.lunsford@outlook.com>
2019-03-08 22:24:24 -0600

    Update: added code to check if logged in user was in the DB, if not a new user is created.

--- restaurant_menu/finalProject.py
+++ restaurant_menu/finalProject.py
@@ -134,6 +134,13 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
+    # see if user exists, if not make a new User
+    user_id = getUserId(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
