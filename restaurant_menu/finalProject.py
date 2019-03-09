6ced37d08956730800afb0ff887b91b80bd8636d
Jon Lunsford <jon.lunsford@outlook.com>
2019-03-08 21:29:52 -0600

    Feature: added function to add a user to the login session.

--- restaurant_menu/finalProject.py
+++ restaurant_menu/finalProject.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, Restaurant, MenuItem, User
 
 # imports for anti-forgery state token
 from flask import session as login_session
@@ -290,6 +290,15 @@ def deleteMenuItem(restaurant_id, menu_id):
     else:
         return render_template('deleteMenuItem.html', restaurant_id=restaurant_id, item=deleteItem)
 
+
+def createUser(login_session):
+    newUser = User(name = login_session['username'],
+                   email= login_session['email'],
+                   picture= login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
 
 
 
