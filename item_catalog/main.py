6a4a5232d0e039410f8eda8877c7c5a6e0bde783
Jon Lunsford <jon.lunsford@outlook.com>
2019-03-31 17:27:57 -0500

    Update: added routes to handle urls for CRUD operations on the database.

--- item_catalog/main.py
+++ item_catalog/main.py
@@ -27,12 +27,84 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# AUTH GOES HERE
+
+
 # API ENDPOINTS GO HERE
 
 
-@app.route("/")
-def index():
-    return render_template('showItems.html')
+# CATEGORY ROUTES GO HERE
+
+@app.route("/categories/")
+def showCategories():
+    """return all categories in the database"""
+    pass
+
+
+@app.route("/category/<int:id>/")
+def getCategory(id):
+    """return a specific category matching the category.id"""
+    pass
+
+
+@app.route("/category/new/", methods=['GET', 'POST'])
+def newCategory():
+    """create a new category in the database"""
+    pass
+
+
+@app.route("/category/<int:id>/edit/", methods=['GET', 'POST'])
+def editCategory(id=id):
+    """edit a category in the database"""
+    pass
+
+
+@app.route("/category/<int:id>/delete/", methods=['GET', 'POST'])
+def deleteCategory(id=id):
+    """delete a category from the database"""
+    pass
+
+
+
+# ITEM ROUTES GO HERE
+
+@app.route("/category/<int:id>/items/")
+def showItems(id):
+    """return all items in the database"""
+    pass
+
+
+@app.route("/item/<int:id>/")
+def getItem(id):
+    """return a specific item in the database"""
+    pass
+
+
+@app.route("/categories/<int:id>/new/", methods=['GET', 'POST'])
+def newItem():
+    """create a new item in the datasbase"""
+    pass
+
+
+@app.route("/item/<int:id>/edit/", methods=['GET', 'POST'])
+def editItem(id=id):
+    """edit an item in the database"""
+    pass
+
+
+@app.route("/item/<int:id>/delete/", methods=['GET', 'POST'])
+def deleteItem(id=id):
+    """delete an item from the database"""
+    pass
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
